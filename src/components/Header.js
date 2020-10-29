9ac384f31777c7c0eaa10123d786c0ad03918bf6
Andrew Stene <ajstene@ksu.edu>
2020-10-29 16:37:52 -0500

    Started to work on the components used by all

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import cow from'./brtf_full-color.png';
+import './Header.css';
+
+function Header()
+{
+    return (
+        <div>
+            <img src= {cow} alt="moo"/>
+
+        </div>
+    );
+}
+
+export default Header;
