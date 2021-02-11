d5583476b276b04be8c43ff778ef750af4d4f465
andrewStene <38635446+andrewStene@users.noreply.github.com>
2021-02-11 16:24:37 -0600

    Work?

--- src/NamePage.js
+++ src/NamePage.js
@@ -1,32 +1,10 @@
 /**
- *  NamePage.js
- *  Copyright (C) 2021  Andrew Stene
- *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *   
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * NamePage.js
  */
 import React from 'react';
 import { Button, TextField } from '@material-ui/core';
 import { Link } from 'react-router-dom'
 import './NamePage.css'
-
-function validate(name)
-{
-    return {
-        name : name.length === 0
-    };
-}
-
 
 /**
  * The react component that allows the user to input a name for
@@ -64,22 +42,8 @@ class NamePage extends React.Component
     */
    updateParent(value)
    {   
-    if(!this.notBlank())
-    {
-        value.preventDefault();
-        return;
-    }
         this.props.setName(value);
         this.setState({name:value});
-
-      
-   }
-
-   notBlank()
-   {
-    const errors = validate(this.state.name);
-    const isDisabled = Object.keys(errors).some(x => errors[x]);
-    return !isDisabled;
    }
 
    /**
