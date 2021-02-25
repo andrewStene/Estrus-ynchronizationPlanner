be12c3edd4defb5ad5bc18803c71043b7e1b6ebe
Ben Amos <bcamos@ksu.edu>
2021-02-24 20:51:46 -0600

    Switched to Radio Buttons on Selection Page

--- src/SelectionPage.js
+++ src/SelectionPage.js
@@ -21,6 +21,9 @@ import RadioGroup from '@material-ui/core/RadioGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormControl from '@material-ui/core/FormControl';
 import FormLabel from '@material-ui/core/FormLabel';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
 import { Button} from '@material-ui/core';
 import { Link } from 'react-router-dom'
 import { Database } from './Database.js';
@@ -39,18 +42,19 @@ class SelectionPage extends React.Component
   {
     super(props);
 
-    this.state=
+    this.state =
     {
-      breed:"",
-      systemType:"",
-      cowType:"",
+      breed:      props.breed,
+      systemType: props.systemType,
+      cowType:    props.cowType,   
       
       /** @type {Database} */ database: this.props.database
     }
-    this.updateParentCowType = this.updateParentCowType.bind(this);
-    this.updateParentBreed = this.updateParentBreed.bind(this);
+   
+    this.updateParentCowType    = this.updateParentCowType.bind(this);
+    this.updateParentBreed      = this.updateParentBreed.bind(this);
     this.updateParentSystemType = this.updateParentSystemType.bind(this);
-    this.verifyInput = this.verifyInput.bind(this);
+    this.verifyInput            = this.verifyInput.bind(this);
   }
 
   /**
@@ -82,14 +86,13 @@ class SelectionPage extends React.Component
   * @param {The breed of cattle the user has selected} breed 
   */
   updateParentBreed(breed)
-  {
-    this.setState({breed:breed});
-    this.props.setBreed(breed);
-
-  }
+  {    
+     this.setState({breed: breed});
+     this.props.setBreed(breed);
+  }  
 
   /**
-  * Function to update both the state of the SelectionType
+   *  Function to update both the state of the SelectionType
    *  as well as the state contained in App.js
    *  of the System to be used
    * @param {The System to update the state to} sys 
@@ -117,46 +120,60 @@ class SelectionPage extends React.Component
   render()
   {
     const breeds      = this.state.database.GetDatabaseListElements( Database.DATABASE_LIST_TYPE.BREED ).map( 
-                        (breed) => < FormControlLabel 
-                        value   = { Database.DATABASE_LIST_NAME.BREED + "-" + breed.Id } 
-                        control = { <Radio /> } 
-                        label   = { breed.Name }/> );
+                        (breed) => < MenuItem
+                        key = { breed.Name }
+                        value   = { Database.DATABASE_LIST_NAME.BREED + "-" + breed.Id }> { breed.Name } </MenuItem > );
 
     const systemTypes = this.state.database.GetDatabaseListElements( Database.DATABASE_LIST_TYPE.SYSTEM_TYPE ).map(
-                        (systemType) => < FormControlLabel 
-                        value        = { Database.DATABASE_LIST_NAME.SYSTEM_TYPE + "-" + systemType.Id } 
-                        control      = { <Radio /> } 
-                        label        = { systemType.Name } /> );    
+                        (systemType) => < MenuItem
+                        key = { systemType.Name } 
+                        value        = { Database.DATABASE_LIST_NAME.SYSTEM_TYPE + "-" + systemType.Id }> { systemType.Name } </MenuItem> );
+
+    let styles = { width:  400,
+                   height: 55 };
     
     return(
         <div>
             <h1>Select { Database.DATABASE_LIST_NAME.BREED } Type</h1>
             <form onSubmit = {(event)=> this.verifyInput(event)}>
-            <FormControl>
-              <FormLabel> { Database.DATABASE_LIST_NAME.BREED } </FormLabel>
-              <RadioGroup aria-label = { Database.DATABASE_LIST_NAME.BREED } name = {Database.DATABASE_LIST_NAME.BREED }
-               value={this.state.breed} onChange={(event)=>this.updateParentBreed(event.target.value)}>
+            <FormControl variant="outlined">
+              <InputLabel> 
+                { Database.DATABASE_LIST_NAME.BREED } 
+              </InputLabel>
+                <Select style = {styles}
+                value = {this.state.breed}
+                onChange={(event) => this.updateParentBreed(event.target.value)}>
+                  <MenuItem value="">
+                      <em>None</em>
+                  </MenuItem>
                 { breeds }
-              </RadioGroup>
+              </Select>
             </FormControl>
             <br/>
             <br/>
-            <FormControl>
-              <FormLabel>Cow or Heifer</FormLabel>
-              <RadioGroup aria-label = "Cow or Heifer" name = "cow" value={this.state.cowType} onChange={(event)=>this.updateParentCowType(event.target.value)}>
-                <FormControlLabel value ="cow" control={<Radio />} label="Cow "/>
-                <FormControlLabel value ="heifer" control={<Radio />} label="Heifer "/>
-              </RadioGroup>
+            <FormControl variant="outlined">
+              <InputLabel>Cow or Heifer</InputLabel>
+              <Select style = {styles} name = "cow" value={this.state.cowType} onChange={(event)=>this.updateParentCowType(event.target.value)}>
+                   <MenuItem value="">
+                      <em>None</em>
+                  </MenuItem>
+                <MenuItem value ="Cow">Cow</MenuItem>
+                <MenuItem value ="Heifer">Heifer</MenuItem>
+              </Select>
             </FormControl>
             <br/>
             <br/>
-            <FormControl>
-              <FormLabel>System Type</FormLabel>
-              <RadioGroup aria-label = { Database.DATABASE_LIST_NAME.SYSTEM_TYPE } name = { Database.DATABASE_LIST_NAME.SYSTEM_TYPE } 
+            <FormControl variant="outlined">
+              <InputLabel> { Database.DATABASE_LIST_NAME.SYSTEM_TYPE } </InputLabel>
+              <Select style = {styles} name = { Database.DATABASE_LIST_NAME.SYSTEM_TYPE } 
                value={this.state.systemType} onChange={(event)=>this.updateParentSystemType(event.target.value)} >
+                 <MenuItem value="">
+                      <em>None</em>
+                  </MenuItem>
                 { systemTypes }                
-              </RadioGroup>
+              </Select>
             </FormControl>
+            <br/>
             <br/>
             <Button className = "sidebysidebutton" component={Link} to="/namepage" variant="contained" size = "large" >Back</Button>
             <Button className = "sidebysidebutton"  component={Link} to="/protocol" variant="contained" size = "large">Next</Button>
