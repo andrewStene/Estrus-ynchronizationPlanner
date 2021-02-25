bb881d03f99a1e0a4913a5a45e68f54caa138f13
Ben Amos <bcamos@ksu.edu>
2021-02-24 21:27:38 -0600

    Some Lorum Ipsum

--- src/ProtocolPage.js
+++ src/ProtocolPage.js
@@ -1,19 +1,5 @@
 /**
- *  ProtocolPage.js
- *  Copyright (C) 2021  Andrew Stene
- *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *   
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * ProtocolPage.js
  */
 import React from 'react';
 import { Button, TextField } from '@material-ui/core';
@@ -24,17 +10,7 @@ import MenuItem from '@material-ui/core/MenuItem';
 import FormHelperText from '@material-ui/core/FormHelperText';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
-import 'date-fns';
-import { format } from 'date-fns';
-import DateFnsUtils from '@date-io/date-fns';
-import Grid from '@material-ui/core/Grid';
-import {
-  MuiPickersUtilsProvider,
-  KeyboardDateTimePicker,
-} from '@material-ui/pickers';
-
 import { Database } from './Database.js';
-
 
 /**
  * Can't think of a good description rn
@@ -58,14 +34,10 @@ class ProtocolPage extends React.Component
         systemType:"",
         cowType:"",
         id:"",
-        startDate: new Date(),
+        description:"",
          /** @type {Database} */ database: this.props.database
        }
-
-       this.updateProtocolId = this.updateProtocolId.bind(this); 
-       this.updateStartDateTime = this.updateStartDateTime.bind(this);
-
-
+       this.updateParent = this.updateParent.bind(this);  
    }
 
     /**
@@ -80,31 +52,21 @@ class ProtocolPage extends React.Component
 
    /**
     * Dont remember what this is atm
-    * @param {The new protocol ID} id 
+    * @param {} value 
     */
-   updateProtocolId(id)
+   updateParent(value)
    {   
-        this.setState({id:id});
-        this.props.setProtocol(id);
+        let protocal = this.state.database.GetObjectByName(value.target.value, Database.DATABASE_LIST_TYPE.PROTOCALS)
+        let description = "";
+        if(protocal != null)
+        {
+            description = protocal.Description;
+        }
+        this.setState({id:value});
+        
+        this.setState({description: description })
    }
 
-   /**
-    * Updates the starting date and time
-    * @param {The start date} date 
-    */
-   updateStartDateTime(date)
-   {
-    console.log(date);
-    this.setState({startDate:new Date(date)});
-    //this.props.setStartDateTime(date);
-   }
-
-
-
-   /**
-    * Not currently implemented
-    * @param {*} event 
-    */
    verifyInput(event)
    {
        event.preventDefualt();
@@ -123,14 +85,13 @@ class ProtocolPage extends React.Component
                                      ( protocal ) => < MenuItem 
                                        value = { protocal.Name } > { protocal.Name } </ MenuItem > );
         
-        let styles = { width: 400,
-                        height: 55 };
+        let styles = { width:  400,
+                       height: 55 };
         return(
             <div>         
             <h1>Select a Protocol</h1>
             <br/>
             <ul>
-
             <li>{this.state.name}</li>
             <li> <b>{ `${ Database.DATABASE_LIST_NAME.BREED }: ` }</b> 
                  { this.state.database.GetNameById( parseIdFromLabel( this.state.breed ), 
@@ -139,7 +100,6 @@ class ProtocolPage extends React.Component
                  { this.state.database.GetNameById( parseIdFromLabel( this.state.systemType ), 
                                                     Database.DATABASE_LIST_TYPE.SYSTEM_TYPE ) }</li>
             <li><b>Cow or Hiefer</b>: {this.state.cowType}</li>
-
             </ul>
             <br/>
             <form>
@@ -147,9 +107,8 @@ class ProtocolPage extends React.Component
               <InputLabel id="demo-simple-select-outlined-label">
                   { Database.DATABASE_LIST_NAME.PROTOCALS } 
               </InputLabel>
-                  <Select style = {styles}
-                   id="protocol"
-                    //value={age}
+                   <Select style = {styles}
+                   id="protocol"                    
                     onChange={this.updateParent}
                     label="Protocol"
                                     >
@@ -159,25 +118,7 @@ class ProtocolPage extends React.Component
                         {recommendedProtocals}                        
                      </Select>
             </FormControl>
-            <br/>
-            <br/>
-            <MuiPickersUtilsProvider utils={DateFnsUtils}>
-                
-            <KeyboardDateTimePicker
-                variant="inline"
-             
-                label="With keyboard"
-                value={this.state.startDate}
-                onChange={(value)=> this.updateStartDateTime(value)}
-                onError={console.log}
-                disablePast
-                format="MM/dd/yyyy hh:mm aa"
-            />
-                
-
-            </MuiPickersUtilsProvider>
-
-            
+            <p><b>Protocal Description:</b> {this.state.description}</p>           
             <br/>
             <br/>
             <Button className = "sidebysidebutton" component={Link} to="/selectionpage" color="defualt" variant="contained" size = "large" >Back</Button>
